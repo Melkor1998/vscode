a5f7a9fe09fae43665ac45b2fab9081fd78a8b54
Shota_22 <84522061+Melkor1998@users.noreply.github.com>
2022-11-24 22:48:23 +0400

    Add files via upload

---
+++ vscode.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cat > install.sh << done
+#!/bin/bash
+if [[ \$(which dpkg &> /dev/null && echo '1') == '1' ]]; then
+	which wget &> /dev/null || sudo apt-get install wget gpg
+	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+	sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+	sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+	rm -f packages.microsoft.gpg
+	sudo apt install apt-transport-https
+	sudo apt update
+	sudo apt install code
+	code
+elif [[ \$(which yum &> /dev/null && echo '1') == '1' ]]; then
+	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+	yum check-update
+	sudo yum install code
+	code
+elif [[ \$(which dnf &> /dev/null && echo '1') == '1' ]]; then
+	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+	dnf check-update
+	sudo dnf install code
+	code
+fi
+rm -rf install.sh
+done
+chmod +x install.sh
+gnome-terminal -x sh -c "./install.sh; bash"
