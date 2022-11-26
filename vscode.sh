acb36eb0f06f0bdcf9cea21067eeb31dc6cd8b9d
Shota_22 <84522061+Melkor1998@users.noreply.github.com>
2022-11-26 15:16:43 +0400

    Update vscode.sh

--- vscode.sh
+++ vscode.sh
@@ -8,24 +8,26 @@ if [[ \$(which dpkg &> /dev/null && echo '1') == '1' ]]; then
 	sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 	sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 	rm -f packages.microsoft.gpg
-	sudo apt install apt-transport-https
+	sudo apt install apt-transport-https -y
 	sudo apt update
-	sudo apt install code
+	sudo apt install code -y
 	code
 elif [[ \$(which yum &> /dev/null && echo '1') == '1' ]]; then
 	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 	yum check-update
-	sudo yum install code
+	sudo yum install code -y
 	code
 elif [[ \$(which dnf &> /dev/null && echo '1') == '1' ]]; then
 	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 	dnf check-update
-	sudo dnf install code
+	sudo dnf install code -y
 	code
 fi
 rm -rf install.sh
+echo 'Thank you for using my Script!
+Author: Shota Melkor from ITVET group 67'
 done
 chmod +x install.sh
 gnome-terminal -x sh -c "./install.sh; bash"
